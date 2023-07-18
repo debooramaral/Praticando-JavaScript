65ac0a86dc85f3e9aeb7ccb72ab3279c086e615e
Débora <amaral.psic17@gmail.com>
2023-07-18 09:32:37 -0400

    aplicando js e corrigindo erro

--- script.js
+++ script.js
@@ -46,9 +46,9 @@ const eventsButton = {
         timer.innerHTML = `${formatTime(hour)}:${formatTime(minute)}:${formatTime(second)}`
     }
 }
-buttonPlay.addEventListener('click', eventsButton.play)
-buttonPause.addEventListener('click', eventsButton.pause)
-buttonStop.addEventListener('click', eventsButton.stop)
+buttonPlay.addEventListener('click', eventsButton.play),
+buttonPause.addEventListener('click', eventsButton.pause),
+buttonStop.addEventListener('click', eventsButton.stop),
 
 function formatTime(timer) {
     if(timer < 10){
@@ -112,29 +112,67 @@ document.querySelector('ul').addEventListener('click', (e) => {
 const currentPlayer = document.querySelector(".currentPlayer");
 
 let selected;
-let player = "X";
+let player = "X"; //iniciando com o X
 let positions = [
-    [1, 2, 3]
-    [4, 5, 6]
-    [7, 8, 9]
-    [1, 4, 7]
-    [2, 5, 8]
-    [3, 6, 9]
-    [1, 5, 9]
-    [3, 5, 7]
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7],
 ]; //posições possiveis para ter um ganhador
 
 function init(){
-    selected = [];
+    selected = []; //inicia vazio
 
-    currentPlayer.innerHTML = `JOGADOR DA VEZ: ${player}`;
+    currentPlayer.innerHTML = `JOGADOR DA VEZ: ${player}`; //inicia com o X
 
-    document.querySelectorAll(".game button").forEach((item) => {
-        item.innerHTML = "";
-        item.addEventListener("click", newMove);
+    document.querySelectorAll(".game button").forEach((item) => { //documento que seleciona todos os botoes da div game
+        item.innerHTML = ""; //cada botão inicializado, inicia vazio, sem nenhum preenchimento quando atualiza a pagina 
+        item.addEventListener("click", newMove); //emprega-se um evento ao clicar
     });
 }
 
 init();
 
-//4:10
+function newMove(e) { //cria-se a função que recebe o evento 
+    const index = e.target.getAttribute("data-i") //ao clicar no atributo colocado no html, retorna o valor do botao
+    e.target.innerHTML = player; //passa a informação do player para o botão
+    e.target.removeEventListener("click", newMove) //remove o evento clique do botão da função
+    selected[index] = player; //armazena no array do botão os itens ja selecionados, o player x ou a bolinha, aquele em que for selecionado
+
+    setTimeout(() => { //função que será executada mais abaixo
+        check();
+    }, [100]);
+
+    player = player === "X" ? "O" : "X"; //depois disso troca-se o player
+    currentPlayer.innerHTML = `JOGADOR DA VEZ: ${player}`; //a cada momento troca-se o current player, atribuindo um novo player
+} 
+
+function check(){ //executa ao final do escopo acima, linhas 150 e 151
+    let playerLastMove = player === "X" ? "O" : "X"; //utiliza o ultimo player que jogou
+
+    const items = selected //mapear os itens selecionados
+    .map((item, i) => [item, i]) //gerando um novo array com x, o ou x e o index deste item
+    .filter((item) => item[0] === playerLastMove) //filtro o novo array, verificando quais itens selecionados batem com o ultimo player
+    .map((item) => item[1]); //mapear apenas o index
+
+    for (pos of positions){
+        if (pos.every((item) => items.includes(item))){
+            alert("O jogador '" + playerLastMove + "' GANHOU ! õ//");
+            init();
+            return;
+        }
+    }
+
+    if(selected.filter((item) => item).length === 9){
+        alert("DEU EMPATE !");
+        init();
+        return;
+    }
+}
+
+//há linhas em que ficam dando erro, imagino que por causa do evento de lista que se 'repete'
+//linha 49 .. verificar o pq
